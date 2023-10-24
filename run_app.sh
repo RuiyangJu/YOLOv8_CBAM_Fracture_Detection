f482613f8d0b40bbe9edc9306af751f2b5be0557
RuiYang Ju <102137894+RuiyangJu@users.noreply.github.com>
2023-10-24 21:09:05 +0800

    Update and rename download.sh to run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,6 @@
+# download example model
 wget https://www.dropbox.com/scl/fi/8yvippxlk0vnh4h06hhch/example_model.zip?rlkey=k52ajv6p98qu99brn5k98q273&dl=0 -O ./YOLOv8_new/example_model.zip
 unzip ./YOLOv8_new/example_model.zip -d ./YOLOv8_new
+
+# run app
+streamlit run app.py  
